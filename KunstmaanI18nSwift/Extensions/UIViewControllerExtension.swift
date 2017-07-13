dda8adadf0099c94850d8e263cd7db1a4e59b754
Daan Poron <daan@poron.be>
2017-07-13 11:55:59 +0200

    conflicting issues with overriding viewDidDissapear on UITabBarController
    when overriding viewDidDissapear in UITabBarController …
    viewDidDissapear won’t be triggered on the current active
    viewController anymore

--- KunstmaanI18nSwift/Extensions/UIViewControllerExtension.swift
+++ KunstmaanI18nSwift/Extensions/UIViewControllerExtension.swift
@@ -24,17 +24,51 @@ extension UIViewController {
         let originalWillAppearSelector = #selector(self.viewWillAppear(_:))
         let swizzledWillAppearSelector = #selector(self.i18n_viewWillAppear(_:))
         Utils.swizzle(self, originalWillAppearSelector, swizzledWillAppearSelector)
+        
+        let originalDidDisappearrSelector = #selector(self.viewDidDisappear(_:))
+        let swizzledDidDisappearSelector = #selector(self.i18n_viewDidDisappear(_:))
+        Utils.swizzle(self, originalDidDisappearrSelector, swizzledDidDisappearSelector)
     }
     
     func i18n_viewWillAppear(_ animated: Bool) {
         self.i18n_viewWillAppear(animated)
         
+        self.updateTranslations()
+        NotificationCenter.default.addObserver(self, selector: #selector(UITabBarController.updateTranslations), name: I18n.Events.onChange, object: nil)
+    }
+    
+    func i18n_viewDidDisappear(_ animated: Bool) {
+        self.i18n_viewDidDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: I18n.Events.onChange, object: nil)
+    }
+    
+    @objc
+    private func updateTranslations() {
         if let i18nTabKey = self.i18nTabKey {
             self.tabBarItem?.title = I18n.localizedString(forKey: i18nTabKey)
         }
         
         if let i18nNavKey = self.i18nNavKey {
             self.navigationItem.title = I18n.localizedString(forKey: i18nNavKey)
+        }
+        
+        if let navVC = self as? UINavigationController {
+            for vc in navVC.viewControllers {
+                if let i18nNavKey = vc.i18nNavKey {
+                    vc.navigationItem.title = I18n.localizedString(forKey: i18nNavKey)
+                }
+            }
+        }
+        
+        if let tabVC = self as? UITabBarController {
+            if let viewControllers = tabVC.viewControllers {
+                for vc in viewControllers {
+                    if let i18nTabKey = vc.i18nTabKey {
+                        vc.tabBarItem?.title = I18n.localizedString(forKey: i18nTabKey)
+                    }
+                }
+            }
         }
     }
     
